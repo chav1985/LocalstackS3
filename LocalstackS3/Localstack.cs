84dd5b84a6164f5c646d4c65a142211253aa4eaf
Carlos Viana <carlos.henrique1985@hotmail.com>
2022-06-24 15:04:53 -0300

    Add project files.

---
+++ LocalstackS3/Localstack.cs
@@ -0,0 +1,56 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace LocalstackS3
+{
+    public class Localstack
+    {
+        private readonly IAmazonS3 _amazonS3;
+        public Localstack(IAmazonS3 amazonS3)
+        {
+            _amazonS3 = amazonS3;
+        }
+
+        public void StartUp()
+        {
+            Console.WriteLine("Iniciado...");
+            Console.WriteLine("Listando buckets...");
+            ListBucketAsync().GetAwaiter().GetResult();
+            Console.WriteLine("criando novo bucket...");
+            CreateBucketAsync("teste").GetAwaiter().GetResult();
+            Console.WriteLine("Excluindo bucket recem criado...");
+            DeleteBucketsAsync("teste").GetAwaiter().GetResult();
+            Console.WriteLine("Listando buckets novamente...");
+            ListBucketAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Finalizado...");
+            Console.ReadKey();
+        }
+
+        private async Task CreateBucketAsync(string bucketName)
+        {
+            await _amazonS3.PutBucketAsync(new PutBucketRequest
+            {
+                BucketName = bucketName,
+                UseClientRegion = true
+            });
+        }
+
+        private async Task ListBucketAsync()
+        {
+            var response = await _amazonS3.ListBucketsAsync();
+
+            foreach (var bucket in response.Buckets)
+            {
+                Console.WriteLine($"Nome do bucket: {bucket.BucketName}");
+            }
+        }
+
+        private async Task DeleteBucketsAsync(string bucketName)
+        {
+            await _amazonS3.DeleteBucketAsync(new DeleteBucketRequest { BucketName = bucketName });
+        }
+    }
+}
+
---
+++ LocalstackS3/Program.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LocalstackS3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var amazonS3 = new AmazonS3Client(new BasicAWSCredentials("abc", "def"), new AmazonS3Config
+            {
+                ServiceURL = "http://localhost:4566",
+                ForcePathStyle = true,
+                UseHttp = true
+            });
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<Localstack>()
+                .AddScoped(typeof(IAmazonS3), provider => amazonS3)
+                .BuildServiceProvider();
+
+            var localstack = serviceProvider.GetService<Localstack>();
+            localstack.StartUp();
+        }
+    }
+}
