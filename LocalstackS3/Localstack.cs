d7968fe21d186390486a007e6ab7d1c429b055d7
Carlos Viana <carlos.henrique1985@hotmail.com>
2022-06-25 10:32:24 -0300

    adicionado mais algumas funcionalidades do s3 utilizando localstack

--- LocalstackS3/Localstack.cs
+++ LocalstackS3/Localstack.cs
@@ -1,6 +1,8 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace LocalstackS3
@@ -15,17 +17,41 @@ namespace LocalstackS3
 
         public void StartUp()
         {
-            Console.WriteLine("Iniciado...");
-            Console.WriteLine("Listando buckets...");
-            ListBucketAsync().GetAwaiter().GetResult();
-            Console.WriteLine("criando novo bucket...");
-            CreateBucketAsync("teste").GetAwaiter().GetResult();
-            Console.WriteLine("Excluindo bucket recem criado...");
-            DeleteBucketsAsync("teste").GetAwaiter().GetResult();
-            Console.WriteLine("Listando buckets novamente...");
-            ListBucketAsync().GetAwaiter().GetResult();
-            Console.WriteLine("Finalizado...");
-            Console.ReadKey();
+            try
+            {
+                Console.WriteLine("Iniciado...");
+                Console.WriteLine("Listando buckets...");
+                ListBucketAsync().GetAwaiter().GetResult();
+
+                Console.WriteLine("criando novo bucket...");
+                CreateBucketAsync("teste").GetAwaiter().GetResult();
+
+                PutObjectAsync("..\\..\\..\\..\\teste_0.txt", "teste").GetAwaiter().GetResult();
+                PutObjectAsync("..\\..\\..\\..\\teste_1.txt", "teste").GetAwaiter().GetResult();
+                PutObjectAsync("..\\..\\..\\..\\teste_2.txt", "teste").GetAwaiter().GetResult();
+                PutObjectAsync("..\\..\\..\\..\\teste_3.txt", "teste").GetAwaiter().GetResult();
+                PutObjectAsync("..\\..\\..\\..\\teste_4.txt", "teste").GetAwaiter().GetResult();
+
+                var lstKeys = ListObjectsAsync("teste").GetAwaiter().GetResult();
+
+                DeleteObjectsAsync("teste", lstKeys).GetAwaiter().GetResult();
+
+                ListObjectsAsync("teste").GetAwaiter().GetResult();
+
+                Console.WriteLine("Excluindo bucket recem criado...");
+                DeleteBucketsAsync("teste").GetAwaiter().GetResult();
+
+                Console.WriteLine("Listando buckets novamente...");
+                ListBucketAsync().GetAwaiter().GetResult();
+
+                Console.WriteLine("Finalizado...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao acessar bucket: {ex.Message}");
+                throw;
+            }
         }
 
         private async Task CreateBucketAsync(string bucketName)
@@ -50,6 +76,38 @@ namespace LocalstackS3
         private async Task DeleteBucketsAsync(string bucketName)
         {
             await _amazonS3.DeleteBucketAsync(new DeleteBucketRequest { BucketName = bucketName });
+        }
+
+        private async Task PutObjectAsync(string path, string bucketName)
+        {
+            StreamWriter sw = new StreamWriter(path);
+            sw.WriteLine("teste");
+            sw.WriteLine("outro teste");
+            sw.WriteLine("mais um teste");
+            sw.Close();
+
+            await _amazonS3.PutObjectAsync(new PutObjectRequest { BucketName = bucketName, FilePath = path });
+        }
+
+        private async Task<List<string>> ListObjectsAsync(string bucketName)
+        {
+            List<string> lstKeys = new List<string>();
+            var response = await _amazonS3.ListObjectsAsync(bucketName);
+
+            foreach (var item in response.S3Objects)
+            {
+                Console.WriteLine($"objeto: {item.Key}");
+                lstKeys.Add(item.Key);
+            }
+            return lstKeys;
+        }
+
+        private async Task DeleteObjectsAsync(string bucketName, List<string> lstKeys)
+        {
+            foreach (var item in lstKeys)
+            {
+                await _amazonS3.DeleteObjectAsync(new DeleteObjectRequest { BucketName = bucketName , Key = item});
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Necessário Docker
 
+Executar o setup na pasta docker do projeto
+
 Projeto simples em .NET Core para manipular o s3 utilizando o Localstack
